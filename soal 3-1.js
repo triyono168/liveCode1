a530522cc8fb757ea4132c64901195bcbc51a82e
triyono168 <triyono16893@gmail.com>
2019-08-26 16:02:44 +0700

    Add files via upload

---
+++ soal 3-1.js
@@ -0,0 +1,6 @@
+function addTitle (name1, name2, name3, name4) {
+ var mr='Mr.'
+
+return mr+' '+name1+','+' '+mr+' '+name2+','+' '+mr+' '+name3+','+' '+mr+' '+name4
+}
+console.log(addTitle('a', 'b', 'c', 'd'))
