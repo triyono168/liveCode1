8049b5813d356de22d5ec1e37383fc834dc4dcbb
triyono168 <triyono16893@gmail.com>
2019-08-26 15:53:17 +0700

    Add files via upload

---
+++ soal no 2.js
@@ -0,0 +1,18 @@
+var nama ="c";
+if (nama [0] == "a"){
+  console.log("Masuk group pertama")
+}else if (nama [0]== "b" ){
+  console.log("Masuk group pertama")
+}else if (nama [0] == "c"){
+  console.log("Masuk group pertama")
+}else if (nama [0]== "d"){
+  console.log("Masuk group pertama")
+}else if (nama [0]== "e"){
+  console.log("Masuk group kedua")
+}else if (nama [0]== "f"){
+  console.log("Masuk group kedua")
+}else if (nama [0]== "g"){
+  console.log("Masuk group kedua")
+}else if (nama [0]== "h"){
+  console.log("Masuk group kedua")
+}
